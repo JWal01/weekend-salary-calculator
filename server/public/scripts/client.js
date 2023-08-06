16647956520410088f60780ca6d919fe1829766c
Jeff Walter <Jeff.Walter@me.com>
2023-08-05 19:19:22 -0500

    added code to find correct monthly cost

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,84 +1,51 @@
+
 console.log('script sourced');
 
-let maxCost = 20000;
-//let monthCost = annualSal % 2;
-let annualSalData = [];
+  let maxCost = 20000;
+  let annualSalData = [];
+  let totalAnnualSal = 0; 
 
+  function submitForm(event) {
+    console.log('submitForm');
+    event.preventDefault();
 
+    let firstName = document.querySelector('#firstName').value;
+    let lastName = document.querySelector('#lastName').value;
+    let idNum = document.querySelector('#ID').value;
+    let title = document.querySelector('#title').value;
+    let annualSal = parseInt(document.querySelector('#annualSalary').value);
 
-function submitForm(event){
-  console.log('submitForm');
-  event.preventDefault();
-  
-  let firstName = document.querySelector('#firstName').value;
-  
+    let employeeDataTable = document.querySelector('#employeeData');
 
-  let lastName = document.querySelector('#lastName').value;
-  
-
-  let idNum = document.querySelector('#ID').value;
-  
-
-  let title = document.querySelector('#title').value;
-  
-
-  let annualSal = parseInt(document.querySelector('#annualSalary').value);
-  
-
-  let employeeDataTable = document.querySelector('#employeeData');
-
-  employeeDataTable.innerHTML += `
-    <tr>
+    employeeDataTable.innerHTML += `
+      <tr>
         <td>${firstName}</td>
         <td>${lastName}</td>
         <td>${idNum}</td>
         <td>${title}</td>
         <td>${annualSal}</td>
         <td><button onclick="removeRow(event)">Delete</button></td>
-     
-  `;
+      </tr>
+    `;
 
-  function addSal(annualSal) {
-    annualSal = document.getElementById('annualSalary').value;
-    annualSalData.push(annualSal);  
+    annualSalData.push(annualSal);
     console.log(annualSalData);
-    return false;
-  }
-addSal();
 
+    totalAnnualSal += annualSal;
 
+    let monthlyCost = totalAnnualSal / 12;
+    document.querySelector('#monthlyCost').innerText = monthlyCost;
 
-
-   let monthlyCost = Number(annualSal / 12);
-   
-        document.querySelector('#monthlyCost').innerHTML = monthlyCost;
-    if(monthlyCost > maxCost){
-      document.querySelector('#monthlyCost').backgroundColor = 'red';
+    if (monthlyCost > maxCost) {
+      document.querySelector('#monthlyCost').style.backgroundColor = 'red';
+    } else {
+      document.querySelector('#monthlyCost').style.backgroundColor = 'transparent';
     }
-      
-  
 
-console.log(monthlyCost);
+    console.log(monthlyCost);
+  }
 
-let sum = 0;
-
-// iterate over each item in the array
-for (let i = 0; i < annualSalData.length; i++ ) {
-  sum += annualSalData[i];
-}
-
-console.log(sum) // 15
-
-};
-
-
-
-
-
-
-function removeRow(event){
-  event.target.closest('tr').remove();
-};
-
-
+  function removeRow(event) {
+    event.target.closest('tr').remove();
+  }
 
